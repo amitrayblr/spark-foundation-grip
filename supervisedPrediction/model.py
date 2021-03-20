cd9cd7dce556bc01e026e84d15353319d302f8e7
amitrayblr <amitray.blr@gmail.com>
2021-03-20 20:40:04 +0530

    cleaned code

--- supervisedPrediction/model.py
+++ supervisedPrediction/model.py
@@ -22,6 +22,7 @@ x = data.iloc[:, :-1].values
 y = data.iloc[:, 1].values  
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0) 
 
+# Training the model
 regressor = LinearRegression()  
 regressor.fit(x_train, y_train) 
 
